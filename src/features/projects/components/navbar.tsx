f08df28ee006354ae956066d708afcf9fa5cb124
pritamawatade <pritamawatade5@gmail.com>
2026-08-02 23:30:20 +0530

    Implement project renaming functionality with optimistic updates and tooltip feature

--- src/features/projects/components/navbar.tsx
+++ src/features/projects/components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -13,12 +15,49 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
+import { useProject, useRenameProject } from "../hooks/use-projects";
+import { useState } from "react";
+import { LoaderIcon, CloudCheck } from "lucide-react"
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@/components/ui/tooltip";
+import { formatDistanceToNow } from "date-fns"
 
 const font = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 export const Navbar = ({ projectId }: { projectId: Id<"projects"> }) => {
+
+
+  const project = useProject(projectId);
+  const renameProject = useRenameProject(projectId);
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [name, setName] = useState(project?.name ?? "");
+
+  const handleStartRenaming = () => {
+    if (!project) return;
+    setName(project.name);
+    setIsRenaming(true);
+  }
+
+  const handleSubmit = () => {
+    if (!project) return;
+    const trimmedName = name.trim();
+    if (!trimmedName || trimmedName.length === 0 || trimmedName === project.name) {
+      setIsRenaming(false);
+      return;
+    }
+    renameProject({ id: projectId, name: trimmedName });
+    setIsRenaming(false);
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    } else if (e.key === "Escape") {
+      setIsRenaming(false);
+    }
+  }
+
   return (
     <div className="flex items-center justify-between gap-x-2 p-2 border-b bg-sidebar">
       <div className="flex items-center gap-x-2">
@@ -43,12 +82,59 @@ export const Navbar = ({ projectId }: { projectId: Id<"projects"> }) => {
             </BreadcrumbItem>
             <BreadcrumbSeparator className="ml-0! mr-1" />
             <BreadcrumbItem>
-              <BreadcrumbPage className="text-sm cursor-pointer hover:text-primary font-medium max-w-40 truncate">
-                Demo Project
-              </BreadcrumbPage>
+              {
+                isRenaming ? (
+                  <input
+                    autoFocus
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    onFocus={(e) => e.target.select()}
+                    onBlur={handleSubmit}
+                    onKeyDown={handleKeyDown}
+                    className="text-sm bg-transparent text-foreground outline-none focus-ring-1 focus-ring-inset focus-ring-ring font-medium max-w-40 truncate"
+                  />
+                ) : (
+                  <BreadcrumbPage
+                    onClick={handleStartRenaming}
+                    className="text-sm cursor-pointer hover:text-primary font-medium max-w-40 truncate">
+                    {project?.name ?? "Loading..."}
+                  </BreadcrumbPage>
+                )
+              }
+
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+
+        <TooltipProvider delayDuration={0}>
+          {
+            project?.importStatus === "importing" ? (
+
+              <Tooltip>
+
+                <TooltipTrigger>
+                  <LoaderIcon className="animate-spin h-4 w-4 text-muted-foreground" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Importing project...</p>
+                </TooltipContent>
+              </Tooltip>
+            ) : (
+              project?.updatedAt && (
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <CloudCheck className="h-4 w-4 text-muted-foreground" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Saved {" "}
+                    {formatDistanceToNow(project?.updatedAt ?? 0, { addSuffix: true })}
+                  </TooltipContent>
+                </Tooltip>
+              )
+            )
+          }
+        </TooltipProvider>
 
 
       </div>
